b63220d208517f3979fa86241f770aea26e7632c
VipinVIP <kpvipin33@gmail.com>
2021-02-17 00:01:45 +0530

    integrating Portis to project (naive methoed)

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,9 @@
 import styles from "../../styles/Nav.module.sass";
+import Portis from '@portis/web3';
+import Web3 from 'web3';
+
+const portis = new Portis('1b0ac6e5-efa2-481a-a7d7-188b24722233', 'mainnet');
+
 
 const Nav = ({ web3 }) => {
   const onClick = async () => {
@@ -8,6 +13,17 @@ const Nav = ({ web3 }) => {
       console.log(e);
     }
   };
+ const handlePortis=async()=>{
+  const web3 = new Web3(portis.provider);
+  try {
+
+    await web3.eth.getAccounts((error, accounts) => {
+      console.log(accounts);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+ };
   return (
     <div className={styles.nav}>
       <h1 className={styles.logo}>
@@ -18,6 +34,9 @@ const Nav = ({ web3 }) => {
       <ul className={styles.navOptions}>
         <button type="button" onClick={onClick}>
           <li className={styles.signup}>Connect to Wallet</li>
+        </button>
+        <button type="button" onClick={handlePortis}>
+          <li className={styles.signup}>Connect to Portis</li>
         </button>
       </ul>
     </div>
