043dc9829206674eea9ccec10cb8e35540242385
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-02-19 16:57:28 +0530

    ✨ minor tweaks 🤝 removes redundant files

--- src/contracts/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import info from "./info.json";
-
-const factory = new web3.eth.Contract(
-  info.factoryInterface,
-  info.factoryAddress
-);
-
-export default factory;
--- src/contracts/question.js
+++
@@ -1,7 +0,0 @@
-// Preconfigured instance of the contract
-import web3 from "./web3";
-import info from "./info.json";
-
-export default async function questionInstance(deployedAddress) {
-  return await new web3.eth.Contract(info.questionInterface, deployedAddress);
-}
