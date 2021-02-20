5255d3c9a6daa0f8baeef5000a2f60cf60d6e4d7
VipinVIP <kpvipin33@gmail.com>
2021-02-20 20:06:45 +0530

    Almost integred

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
 import styles from "../../styles/UserProfile.module.sass";
 
+const info = require("../EnableWeb3/info.json");
+
 const getBalance = async (web3, walletAddress) => {
   return await web3.eth.getBalance(walletAddress);
 };
 
-export const UserProfile = ({ walletAddress, factory, web3 }) => {
+const mes = async (walletAddress, question, marketaddress) => {
+  return await question.getPastEvents("staked", {
+    filter: {
+      _market: [marketaddress],
+      _user: [walletAddress],
+    },
+    fromBlock: "23400000",
+    toBlock: "latest",
+  });
+};
+
+export const UserProfile = ({ walletAddress, markets, factory, web3 }) => {
   const [balance, setBalance] = useState(0);
+  const [myRecents, setMyRecents] = useState([]);
+
   // const [addressList, setAddressList] = useState([]);
+
+  useEffect(() => {
+    console.log(myRecents);
+  }, [myRecents]);
 
   // if (
   //   web3 !== undefined &&
@@ -39,6 +58,38 @@ export const UserProfile = ({ walletAddress, factory, web3 }) => {
     }
   }, [web3, factory, walletAddress]);
 
+  useEffect(() => {
+    if (markets && walletAddress) {
+      markets.forEach((item) => {
+        // console.log(item.details.address);
+        const question = new web3.eth.Contract(
+          info.questionInterface,
+          item.details.address
+        );
+        mes(walletAddress, question, item.details.address)
+          .then((obj) => {
+            let amount = [];
+            obj.forEach((item) => {
+              // console.log(item);
+              amount[0] = item.address;
+              const optid = parseInt(item.returnValues._optionId) + 1;
+              if (amount[optid] === undefined) {
+                amount[optid] = parseInt(item.returnValues._amount);
+              } else {
+                amount[optid] += parseInt(item.returnValues._amount);
+              }
+            });
+            // console.log(amount);
+            if (amount.length !== 0) {
+              // console.log(amount);
+              setMyRecents([...myRecents, amount]);
+            }
+          })
+          .catch((err) => console.log(err));
+      });
+    }
+  }, [markets, walletAddress]);
+
   return walletAddress ? (
     <>
       <div>
@@ -46,6 +97,9 @@ export const UserProfile = ({ walletAddress, factory, web3 }) => {
           <h2>Dashboard</h2>
           <div>Current Address :{walletAddress}</div>
           <div>Current balance :{balance}</div>
+          <br />
+          <h4>Recent activities</h4>
+          <b />
         </div>
       </div>
     </>
