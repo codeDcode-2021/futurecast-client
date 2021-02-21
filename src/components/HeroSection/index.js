56a424db9d7f5d9b5b5ece13a9139a7c9087fbae
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-02-21 17:50:59 +0530

    ✨ adds icons ✨ updates landing page text

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,33 +1,16 @@
-import styles from '../../styles/HeroSection.module.sass';
+import styles from "../../styles/HeroSection.module.sass";
 
 const HeroSection = () => (
   <div className={styles.container}>
     <div className={styles.textContainer}>
-      <h1 className={styles.heading}>Lorem ipsum dolor sit</h1>
+      <h1 className={styles.heading}>SEER</h1>
       <p className={styles.para}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-        temporibus aliquam sed tempora unde maiores officiis ab soluta nesciunt
-        velit fugiat minima minus, reiciendis libero facere quibusdam nostrum
-        fugit. Omnis?
+        Seers is a decentralized market where users can stake on a question and
+        get exciting and cool returns. Well, how we are different from Augur? We
+        can provide you significantly more returns as we stake on matic staking
+        manager as well as some other clients. What are you waiting for? Lets
+        stake some Matic! 🥳
       </p>
-      <div className={styles.formContainer}>
-        <div className={styles.formGroup}>
-          <input
-            type="input"
-            className={styles.formField}
-            placeholder="E-Mail"
-            name="email"
-            id="email"
-            required
-          />
-          <label htmlFor="email" className={styles.formLabel}>
-            E-Mail
-          </label>
-          <button type="button" className={styles.signup}>
-            Sign up
-          </button>
-        </div>
-      </div>
     </div>
   </div>
 );
