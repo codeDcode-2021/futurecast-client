f777444797970953a37d0a583ba0c0ee7ca12893
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-02-21 18:29:21 +0530

    😆 fixed typo

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -5,7 +5,7 @@ const HeroSection = () => (
     <div className={styles.textContainer}>
       <h1 className={styles.heading}>SEER</h1>
       <p className={styles.para}>
-        Seers is a decentralized market where users can stake on a question and
+        Seer is a decentralized market where users can stake on a question and
         get exciting and cool returns. Well, how we are different from Augur? We
         can provide you significantly more returns as we stake on matic staking
         manager as well as some other clients. What are you waiting for? Lets
