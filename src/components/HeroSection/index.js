a9e3aa942f29af90c8490c007131e6886726fe0b
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-02-21 18:38:57 +0530

    ✨ updates project name

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -3,13 +3,13 @@ import styles from "../../styles/HeroSection.module.sass";
 const HeroSection = () => (
   <div className={styles.container}>
     <div className={styles.textContainer}>
-      <h1 className={styles.heading}>SEER</h1>
+      <h1 className={styles.heading}>FutureCast</h1>
       <p className={styles.para}>
-        Seer is a decentralized market where users can stake on a question and
-        get exciting and cool returns. Well, how we are different from Augur? We
-        can provide you significantly more returns as we stake on matic staking
-        manager as well as some other clients. What are you waiting for? Lets
-        stake some Matic! 🥳
+        FutureCast is a decentralized market where users can stake on a question
+        and get exciting and cool returns. Well, how we are different from
+        Augur? We can provide you significantly more returns as we stake on
+        matic staking manager as well as some other clients. What are you
+        waiting for? Lets stake some Matic! 🥳
       </p>
     </div>
   </div>
